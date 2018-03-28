4671a8802bde2de5703c56fd9acd859aced421d9
Thuleq <kszczyrbak8@gmail.com>
2018-03-28 22:40:19 +0200

    register form rollback

--- src/app/register/register.component.spec.ts
+++ src/app/register/register.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RegisterComponent } from './register.component';
+import { NgForm } from '@angular/forms';
+import { Component, ViewChild } from '@angular/core';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+
+  @ViewChild('form') registerForm: NgForm;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(){
+    console.log(this.registerForm);
   }
 
 }
