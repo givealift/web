d37c08868e82db46bdcbf3ce552c5d2c736827b7
Bartek Grodek <bartgrdk@gmail.com>
2018-04-28 01:19:24 +0200

    use citiesProvider when looking for city

--- src/app/services/city-service.ts
+++ src/app/services/city-service.ts
@@ -5,6 +5,7 @@ import { Observable } from "rxjs/Observable";
 import { of } from "rxjs/observable/of";
 import { tap } from "rxjs/operators/tap";
 import { environment } from "../../environments/environment";
+import { CitiesProvider } from "../providers/cities-provider";
 
 export class City {
     cityId: string;
@@ -27,13 +28,22 @@ export class CityService {
 
     private readonly url: string = `${environment.apiUrl}/city/search/`;
 
-    constructor(private http: HttpClient) {
+    constructor(private http: HttpClient, private citiesProvider: CitiesProvider) {
     }
 
     searchCity(term: string, limit: number = 20): Observable<City[]> {
         if (!term || !term.trim()) {
             return of([]);
         }
+
+        const localMatches: City[] = this.citiesProvider
+            .getCities()
+            .filter(this.startsWith(term));
+
+        if (localMatches.length) {
+            return of(localMatches);
+        }
+
         const fetchedEntriesLimit = limit.toString();
         const params = new HttpParams({
             fromObject: {
@@ -42,5 +52,17 @@ export class CityService {
             }
         });
         return this.http.get<City[]>(this.url, { params });
+    }
+
+    private startsWith = (term: string) => (city: City) => {
+        const [normalisedTerm, normalisedName] = [term, city.name]
+            .map(string =>
+                string
+                    .normalize("NFD") // decompose to letter and diacratic symbol
+                    .replace(/[\u0300-\u036f]/g, "") // remove diacratic symbols
+                    .replace(/\u0142/g, "l") // 'ł' is considers as diffrent letter than 'l', so replace that too
+                    .toLowerCase()
+            )
+        return normalisedName.startsWith(normalisedTerm);
     }
 }
