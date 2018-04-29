9deacb62509194e729beebb312542b69086a0e34
Thuleq <kszczyrbak8@gmail.com>
2018-04-29 18:09:30 +0200

    fixed mocked route list

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -55,11 +55,4 @@ describe('HomeComponent', () => {
     expect(app.title).toEqual('Give-a-lift');
   }));
 
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(HomeComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Witaj w Give-a-lift!');
-  }));
-
 });
