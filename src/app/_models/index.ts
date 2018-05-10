01a97d00ffd896cc84c28595535cb8cb70a74941
Bartek Grodek <bartgrdk@gmail.com>
2018-05-10 14:01:40 +0200

    refactor & fix storing and reading the same credentials
    # Conflicts:
    #	src/app/_models/index.ts

--- src/app/_models/index.ts
+++ src/app/_models/index.ts
@@ -2,10 +2,12 @@ import { City } from "./city";
 import { Route } from "./route";
 import { User } from "./user";
 import { Location } from "./location";
+import { Authentication } from "./authentication"
 
 export {
     City,
     Route,
     User,
-    Location
+    Location,
+    Authentication,
 }
