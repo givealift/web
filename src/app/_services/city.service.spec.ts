2239aacc836db1b6b46e2b8d6cc089f8314ed0bf
Bartek Grodek <bartgrdk@gmail.com>
2018-05-11 16:32:54 +0200

    wrap each test spec in async

--- src/app/_services/city.service.spec.ts
+++ src/app/_services/city.service.spec.ts
@@ -3,9 +3,10 @@ import { TestBed, async } from "@angular/core/testing";
 import { CitiesProvider } from "../_providers/cities-provider";
 import { HttpClientModule, HttpClient } from "@angular/common/http";
 
-let cityService: CityService;
 
 describe("CityService", () => {
+
+    let cityService: CityService;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -27,35 +28,35 @@ describe("CityService", () => {
         jasmine.DEFAULT_TIMEOUT_INTERVAL = 25 * 1000;
     })
 
-    it("#searchCity should return one city if exists", () => {
+    it("#searchCity should return one city if exists", async(() => {
         const cityName = "Warszawa";
         cityService.searchCity(cityName).subscribe(city => {
             expect(city).toBeDefined();
             expect(city.name).toEqual(cityName);
         })
-    })
+    }))
 
-    it("#searchCity should return null if city doesn't exists", () => {
+    it("#searchCity should return null if city doesn't exists", async(() => {
         const cityName = "ZXC";
         cityService.searchCity(cityName).subscribe(city => {
             expect(city).toBe(null);
         })
-    })
+    }))
 
-    it("#searchCities should return array with one item if found", () => {
+    it("#searchCities should return array with one item if found", async(() => {
         const cityName = "Warszawa";
         cityService.searchCities(cityName, 1).subscribe(cities => {
             expect(cities).toEqual(jasmine.any(Array));
             expect(cities.length).toEqual(1);
             expect(cities[0].name).toEqual(cityName);
         })
-    })
+    }))
 
-    it("#searchCities should return empty array if not found", () => {
+    it("#searchCities should return empty array if not found", async(() => {
         const cityName = "ZXC";
         cityService.searchCities(cityName, 1).subscribe(cities => {
             expect(cities).toEqual(jasmine.any(Array));
             expect(cities.length).toEqual(0);
         })
-    })
+    }))
 })
