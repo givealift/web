cc1c99f25788dbd8f084b874c27a7dac6b7aec83
Bartek Grodek <bartgrdk@gmail.com>
2018-05-11 18:54:11 +0200

    fix tests

--- src/app/_services/city.service.spec.ts
+++ src/app/_services/city.service.spec.ts
@@ -1,62 +1,91 @@
 import { CityService } from "./city.service";
-import { TestBed, async } from "@angular/core/testing";
+import { TestBed } from "@angular/core/testing";
 import { CitiesProvider } from "../_providers/cities-provider";
-import { HttpClientModule, HttpClient } from "@angular/common/http";
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 
 describe("CityService", () => {
 
     let cityService: CityService;
+    let httpMock: HttpTestingController;
 
-    beforeEach(async(() => {
+    beforeEach(() => {
         TestBed.configureTestingModule({
             providers: [
                 CityService,
                 CitiesProvider
             ],
             imports: [
-                HttpClientModule
+                HttpClientTestingModule
             ]
-        }).compileComponents()
-    }))
+        });
 
-    beforeEach(() => {
+        httpMock = TestBed.get(HttpTestingController);
         cityService = TestBed.get(CityService);
     })
 
-    beforeAll(() => {
-        jasmine.DEFAULT_TIMEOUT_INTERVAL = 25 * 1000;
-    })
+    afterEach(() => {
+        // Assert that there are no more pending requests.
+        httpMock.verify();
+    });
 
-    it("#searchCity should return one city if exists", async(() => {
+    it("#searchCity('Warszawa') should return one city if exists", () => {
         const cityName = "Warszawa";
         cityService.searchCity(cityName).subscribe(city => {
             expect(city).toBeDefined();
             expect(city.name).toEqual(cityName);
         })
-    }))
 
-    it("#searchCity should return null if city doesn't exists", async(() => {
+        const req = httpMock.expectOne(
+            req => req.params.has("search")
+        );
+
+        const mockedResponse = [{ name: "Warszawa", cityId: 1 }];
+        req.flush(mockedResponse);
+    })
+
+    it("#searchCity('ZXC') should return null if city doesn't exists", () => {
         const cityName = "ZXC";
         cityService.searchCity(cityName).subscribe(city => {
             expect(city).toBe(null);
         })
-    }))
 
-    it("#searchCities should return array with one item if found", async(() => {
+        const req = httpMock.expectOne(
+            req => req.params.get("search") === cityName
+        );
+
+        const mockedResponse = [];
+        req.flush(mockedResponse);
+    })
+
+    it("#searchCities('Wraszawa') should return array with one item if found", () => {
         const cityName = "Warszawa";
         cityService.searchCities(cityName, 1).subscribe(cities => {
             expect(cities).toEqual(jasmine.any(Array));
             expect(cities.length).toEqual(1);
             expect(cities[0].name).toEqual(cityName);
         })
-    }))
 
-    it("#searchCities should return empty array if not found", async(() => {
+        const req = httpMock.expectOne(
+            req => req.params.has("search")
+        );
+
+        const mockedResponse = [{ name: "Warszawa", cityId: 1 }];
+        req.flush(mockedResponse);
+    })
+
+    it("#searchCities('ZXC') should return empty array if not found", () => {
         const cityName = "ZXC";
         cityService.searchCities(cityName, 1).subscribe(cities => {
             expect(cities).toEqual(jasmine.any(Array));
             expect(cities.length).toEqual(0);
         })
-    }))
+
+        const req = httpMock.expectOne(
+            req => req.params.has("search")
+        );
+
+        const mockedResponse = [];
+        req.flush(mockedResponse);
+    })
 })
