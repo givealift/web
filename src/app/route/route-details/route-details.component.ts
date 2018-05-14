79ba4248caf4aec60869cad262328717987bf833
Thuleq <kszczyrbak8@gmail.com>
2018-05-14 15:52:02 +0200

    test fix

--- src/app/route/route-details/route-details.component.ts
+++ src/app/route/route-details/route-details.component.ts
@@ -5,7 +5,7 @@ import { ActivatedRoute } from "@angular/router";
 import { User } from "../../_models";
 import { Route } from "../../_models";
 import { Router } from "@angular/router";
-import {City} from "../../_models/city";
+import { City } from "../../_models/city";
 
 @Component({
   selector: 'app-route-details',
@@ -48,8 +48,9 @@ export class RouteDetailsComponent implements OnInit {
     this.routeService.getById(this.routeId).subscribe(
       routes => {
         this.routes = routes;
-        if ( this.routes.length === 1) {
-          this.routeDetails = this.routes[0]; }
+        if (this.routes.length === 1) {
+          this.routeDetails = this.routes[0];
+        }
         // for ( let route of this.routes ) {
         //   this.routeDetails = route; }
         // this.routeDetails = this.routes.valueOf();
@@ -57,97 +58,41 @@ export class RouteDetailsComponent implements OnInit {
         let userId = this.routeDetails.ownerId;
         let possibleUser = this.userService.getById(userId);
         if (possibleUser != null) {
-          this.userData = possibleUser; }
+          this.userData = possibleUser;
+        }
 
         if (this.routeDetails.from.date === undefined) {
-          console.log('from date===undefined: ' + this.routeDetails.from.date );
+          console.log('from date===undefined: ' + this.routeDetails.from.date);
           this.routeDetails.from.date = null;
         } else {
-          console.log('from date: ' + this.routeDetails.from.date); }
+          console.log('from date: ' + this.routeDetails.from.date);
+        }
 
         if (this.routeDetails.from.placeOfMeeting === undefined) {
-          console.log('from placeOfMeeting===undefined: ' + this.routeDetails.from.placeOfMeeting );
+          console.log('from placeOfMeeting===undefined: ' + this.routeDetails.from.placeOfMeeting);
           this.routeDetails.from.placeOfMeeting = null;
         } else {
-          console.log('from placeOfMeeting: ' + this.routeDetails.from.placeOfMeeting ); }
+          console.log('from placeOfMeeting: ' + this.routeDetails.from.placeOfMeeting);
+        }
 
         if (this.routeDetails.to.placeOfMeeting === undefined) {
-          console.log('to placeOfMeeting===undefined: ' + this.routeDetails.to.placeOfMeeting );
+          console.log('to placeOfMeeting===undefined: ' + this.routeDetails.to.placeOfMeeting);
           this.routeDetails.to.placeOfMeeting = null;
         } else {
-          console.log('to placeOfMeeting: ' + this.routeDetails.to.placeOfMeeting ); }
-
-
-          // buildingNumber: number;
-          // city: City = new City();
-          // date: any;
-          // localizationId: number;
-          // placeOfMeeting: string;
-          // this.routeDetails.stops = [
-          //   {
-          //     "buildingNumber": 11,
-          //     "city": {
-          //       "cityId": "20",
-          //       "name": "Częstochowa",
-          //       "country": "powiat Katowice",
-          //       "province": "śląskie",
-          //       "cityInfo": {
-          //         "cityInfoId": 20,
-          //         "population": 304362,
-          //         "citySize": 165
-          //       }
-          //     },
-          //     "date": "2018-05-13T23:42:49+0000",
-          //     "localizationId": 201,
-          //     "placeOfMeeting": "Krzywa 4"
-          //   },
-          //   {
-          //     "buildingNumber": 16,
-          //     "city": {
-          //       "cityId": "21",
-          //       "name": "Piotrków Trybunalski",
-          //       "country": "powiat Warszawa",
-          //       "province": "mazowieckie",
-          //       "cityInfo": {
-          //         "cityInfoId": 2,
-          //         "population": 1724404,
-          //         "citySize": 517
-          //       }
-          //     },
-          //     "date": "2018-05-14T03:50:00+0000",
-          //     "localizationId": 214,
-          //     "placeOfMeeting": "Skrzywiona 4"
-          //   },
-          //   {
-          //     "buildingNumber": 16,
-          //     "city": {
-          //       "cityId": "22",
-          //       "name": "Łódź",
-          //       "country": "powiat Warszawa",
-          //       "province": "mazowieckie",
-          //       "cityInfo": {
-          //         "cityInfoId": 2,
-          //         "population": 1724404,
-          //         "citySize": 517
-          //       }
-          //     },
-          //     "date": "2018-05-14T03:50:00+0000",
-          //     "localizationId": 214,
-          //     "placeOfMeeting": "Płynna 14"
-          //   }
-          // ];
+          console.log('to placeOfMeeting: ' + this.routeDetails.to.placeOfMeeting);
+        }
 
         if (this.routeDetails.stops === undefined) {
-          console.log('stops===undefined: ' + this.routeDetails.stops );
+          console.log('stops===undefined: ' + this.routeDetails.stops);
           this.routeDetails.stops = null;
         } else {
-          console.log('stops: ' + this.routeDetails.stops );
+          console.log('stops: ' + this.routeDetails.stops);
           this.numberOfStops = this.routeDetails.stops.length;
         }
 
-        if( this.routeDetails.stops !== null ) {
-          for ( let stop of this.routeDetails.stops ) {
-            if ( stop === undefined ) {
+        if (this.routeDetails.stops !== null) {
+          for (let stop of this.routeDetails.stops) {
+            if (stop === undefined) {
               stop = null;
             }
           }
