3464c28ab266ff43bec82cc950eba2fc648190c6
Bartek Grodek <bartgrdk@gmail.com>
2018-05-15 17:36:31 +0200

    add missing imports in tests

--- src/app/route/route-list/route-list.component.spec.ts
+++ src/app/route/route-list/route-list.component.spec.ts
@@ -4,17 +4,35 @@ import { HttpClientModule } from '@angular/common/http';
 import { MaterialModule } from '../../_modules/material.module';
 import { RouteComponent } from '../route/route.component';
 import { DataProviderService } from '../../_services/data-provider.service';
+import { RouteService } from '../../_services/route.service';
+import { ActivatedRoute } from '@angular/router';
+import { CityService } from '../../_services/city.service';
+import { CitiesProvider } from '../../_providers/cities-provider';
 
+const fakeActivatedRoute = {
+  snapshot: { data: {} }
+}
 describe('RouteListComponent', () => {
   let component: RouteListComponent;
   let fixture: ComponentFixture<RouteListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [RouteListComponent, RouteComponent],
-      imports: [HttpClientModule,
-        MaterialModule],
-      providers: [DataProviderService]
+      declarations: [
+        RouteListComponent,
+        RouteComponent
+      ],
+      imports: [
+        HttpClientModule,
+        MaterialModule
+      ],
+      providers: [
+        DataProviderService,
+        RouteService,
+        CityService,
+        CitiesProvider,
+        { provide: ActivatedRoute, useValue: fakeActivatedRoute }
+      ]
     })
       .compileComponents();
   }));
