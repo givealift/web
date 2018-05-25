2a66acef69d78c4fe0fce6acd5be885a40a021d7
Thuleq <kszczyrbak8@gmail.com>
2018-05-25 14:51:05 +0200

    added notification displaying and mocking methods

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -3,6 +3,9 @@ import { AuthService } from '../_services/auth.service';
 import { Router } from '@angular/router';
 import { User } from "../_models";
 import { UserService } from '../_services/user.service';
+import { GalNotification, NewRouteNotification, ReservationNotification } from '../_models/gal-notification';
+import { CitiesProvider } from '../_providers/cities-provider';
+import { CityService } from '../_services/city.service';
 
 @Component({
   selector: 'app-nav',
@@ -13,9 +16,13 @@ export class NavComponent implements OnInit {
   userId: number;
   loggedIn: boolean;
 
-  notifications = new Array<any>();
+  notifications = new Array<GalNotification>();
 
-  constructor(private authService: AuthService, private userService: UserService, private router: Router) { }
+  constructor(private authService: AuthService,
+    private userService: UserService,
+    private router: Router,
+    private cityService: CityService
+  ) { }
 
   ngOnInit() {
     this.loggedIn = this.authService.isAuthenticated();
@@ -23,14 +30,39 @@ export class NavComponent implements OnInit {
       this.loggedIn = loggedIn
     });
 
-    this.notifications.push('a');
-    this.notifications.push('a');
-    this.notifications.push('a');
-    this.notifications.push('a');
+    this.addNewRouteNotification();
+    this.addReservationNotification();
+    this.addNewRouteNotification();
   }
 
   logout(): void {
     this.authService.logout();
     this.router.navigate(["/"]);
+  }
+
+  addNewRouteNotification() {
+    let notification = new GalNotification();
+    notification.routeId = 112;
+    let body = new NewRouteNotification;
+    this.cityService.searchCity("Warszawa").subscribe(
+      city => body.from = city
+    );
+    this.cityService.searchCity("Kraków").subscribe(
+      city => body.to = city
+    );
+    body.date = new Date();
+    notification.newRoute = body;
+
+    this.notifications.push(notification);
+  }
+
+  addReservationNotification() {
+    let notification = new GalNotification();
+    notification.routeId = 112;
+    let body = new ReservationNotification;
+    body.userId = parseInt(localStorage.getItem('id'));
+    notification.reservation = body;
+
+    this.notifications.push(notification);
   }
 }
