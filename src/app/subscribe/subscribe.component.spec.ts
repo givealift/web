9df8eba5327f7a59bd758093fad53eb03dcdc4c8
Bartek Grodek <bartgrdk@gmail.com>
2018-05-28 16:45:06 +0200

    update tests

--- src/app/subscribe/subscribe.component.spec.ts
+++ src/app/subscribe/subscribe.component.spec.ts
@@ -8,6 +8,9 @@ import { AuthService } from '../_services/auth.service';
 import { UserService } from '../_services/user.service';
 import { DataProviderService } from '../_services/data-provider.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CityService } from '../_services/city.service';
+import { CitiesProvider } from '../_providers/cities-provider';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('SubscribeComponent', () => {
   let component: SubscribeComponent;
@@ -20,10 +23,13 @@ describe('SubscribeComponent', () => {
         HttpClientModule,
         MaterialModule,
         FormsModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        RouterTestingModule
       ],
       providers: [
         SubscriptionService,
+        CityService,
+        CitiesProvider,
         AuthService,
         UserService,
         DataProviderService
