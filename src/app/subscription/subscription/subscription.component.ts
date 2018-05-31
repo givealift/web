06c3ae214c557930a4c9d690dac5e66a632b3504
axal25 <emevig@gmail.com>
2018-05-31 02:46:32 +0200

    not done: more work on user-subscriptions, not working subscription

--- src/app/subscription/subscription/subscription.component.ts
+++ src/app/subscription/subscription/subscription.component.ts
@@ -9,10 +9,36 @@ export class SubscriptionComponent implements OnInit {
 
   @Input()
   subData: any;
+  isDataReady: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+    //getFromCity, getToCity //byId
+    //date
+    //notificationType
+    //subscriber //<- subscriberId
+
+    /** Fake'owe dane
+     * TO DO: zamienic i usunac
+     *
+     * **/
+
+    if( this.subData.fromCityId == 1 ) { this.subData.setParameter("fromCityName", "Kraków"); }
+    if( this.subData.fromCityId == 2 ) { this.subData.setParameter("fromCityName", "Warszawa"); }
+    if( this.subData.fromCityId == 3 ) { this.subData.setParameter("fromCityName", "Łódź"); }
+    if( this.subData.fromCityId == 4 ) { this.subData.setParameter("fromCityName", "Katowice"); }
+
+    if( this.subData.toCityId == 1 ) { this.subData.setParameter("toCityName", "Kraków"); }
+    if( this.subData.toCityId == 2 ) { this.subData.setParameter("toCityName", "Warszawa"); }
+    if( this.subData.toCityId == 3 ) { this.subData.setParameter("toCityName", "Łódź"); }
+    if( this.subData.toCityId == 4 ) { this.subData.setParameter("toCityName", "Katowice"); }
+
+    this.subData.setParameter("subscriber", "Nazwisko Imie"); //Znalezc nazwe uzytkownika po Id?
+
+    this.isDataReady = true;
+    console.log("subData = ", this.subData);
+
   }
 
 }
--- src/app/user-profile/user-subscriptions/user-subscriptions.component.ts
+++ src/app/user-profile/user-subscriptions/user-subscriptions.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {User} from "../../_models";
+import { User } from "../../_models";
+import { SubscriptionService } from "../../_services/subscription.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-user-subscriptions',
@@ -9,33 +11,45 @@ import {User} from "../../_models";
 export class UserSubscriptionsComponent implements OnInit {
 
   user: User = new User();
-  userSubs: any = [];
+  usersSubs: any = [];
+  tmpSub: any;
+  isDataReady: boolean = false;
 
-  constructor() { }
+  constructor( private subService: SubscriptionService,
+               private router: Router) { }
 
   ngOnInit() {
+    this.user.userId = parseInt( localStorage.getItem("id") );
 
-    // this.subscriptionService.getByUserId(this.routeId).subscribe(
-    //   route => {
-    //     this.routeDetails = route;
-    //     this.userData = route.galUserPublicResponse;
-    //     this.userService.getPhoto(this.userData.userId)
-    //       .subscribe(photo => {
-    //         let urlCreator = window.URL;
-    //         this.sanitizedPhoto = this.sanitizer.bypassSecurityTrustUrl(urlCreator.createObjectURL(photo));
-    //       })
-    //     this.isDataReady = true;
-    //   },
-    //   error => {
-    //     this.router.navigate['user-routes'];
-    //   }
-    // );
+    this.subService.getUserSubscriptions( this.user.userId.toString() ).subscribe(
+         route => {
+           this.tmpSub = route;
+           if ( this.usersSubs.length !== 0 ) {
+              this.usersSubs.push( this.tmpSub );
+           } else {
+             this.usersSubs.next( this.tmpSub );
+           }
+           this.isDataReady = true;
+         },
+         error => {
+           this.router.navigate['user-profile'];
+         }
+    );
 
+    console.log("userSubs = ", this.usersSubs);
 
-    this.userSubs = [
-      {'subName':'subcription#1', 'from':'Krakow', 'to':'Krakowek'},
-      {'subName':'subcription#2', 'from':'Warszawa', 'to':'Warszawka'}
+    /** zamockowane nada
+     * TO DO: usunac
+     * **/
+    this.usersSubs = [
+      {'date':"2018-06-25T23:30:00.259Z", 'email': "user1@gmail.pl", 'fromCityId': 1, 'notificationyType':"POST", 'subscriber':"101", 'toCityId':2},
+      {'date':"2018-06-25T23:30:00.259Z", 'email': "user1@gmail.pl", 'fromCityId': 1, 'notificationyType':"BOT", 'subscriber':"101", 'toCityId':2},
+      {'date':"2018-06-26T23:45:00.259Z", 'email': "user2@gmail.pl", 'fromCityId': 3, 'notificationyType':"POST", 'subscriber':"101", 'toCityId':4},
+      {'date':"2018-06-26T23:45:00.259Z", 'email': "user2@gmail.pl", 'fromCityId': 3, 'notificationyType':"BOT", 'subscriber':"101", 'toCityId':4}
     ];
+    console.log("STILL MOCKUP\nuserSubs = mockup");
+    console.log("userSubs = ", this.usersSubs);
+
   }
 
 }
