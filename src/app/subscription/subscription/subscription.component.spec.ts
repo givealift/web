fc01ab741ac86842e71289a2604ac6e7c7410102
Thuleq <kszczyrbak8@gmail.com>
2018-06-02 13:38:56 +0200

    test fixes #1, one test still failing

--- src/app/subscription/subscription/subscription.component.spec.ts
+++ src/app/subscription/subscription/subscription.component.spec.ts
@@ -2,6 +2,15 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SubscriptionComponent } from './subscription.component';
 import { MaterialModule } from '../../_modules/material.module';
+import { SubscriptionService } from '../../_services/subscription.service';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from '../../_services/auth.service';
+import { UserService } from '../../_services/user.service';
+import { DataProviderService } from '../../_services/data-provider.service';
+import { CityService } from '../../_services/city.service';
+import { CitiesProvider } from '../../_providers/cities-provider';
+import { RouterTestingModule } from '@angular/router/testing';
+import { RouteService } from '../../_services/route.service';
 
 describe('SubscriptionComponent', () => {
   let component: SubscriptionComponent;
@@ -13,10 +22,18 @@ describe('SubscriptionComponent', () => {
         SubscriptionComponent
       ],
       imports: [
-        MaterialModule
+        MaterialModule,
+        HttpClientModule,
+        RouterTestingModule
       ],
       providers: [
-
+        SubscriptionService,
+        AuthService,
+        UserService,
+        DataProviderService,
+        CityService,
+        CitiesProvider,
+        RouteService
       ]
     })
       .compileComponents();
--- src/app/user-profile/user-subscriptions/user-subscriptions.component.spec.ts
+++ src/app/user-profile/user-subscriptions/user-subscriptions.component.spec.ts
@@ -3,6 +3,15 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { UserSubscriptionsComponent } from './user-subscriptions.component';
 import { SubscriptionComponent } from '../../subscription/subscription/subscription.component';
 import { MaterialModule } from '../../_modules/material.module';
+import { SubscriptionService } from '../../_services/subscription.service';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from '../../_services/auth.service';
+import { UserService } from '../../_services/user.service';
+import { DataProviderService } from '../../_services/data-provider.service';
+import { CityService } from '../../_services/city.service';
+import { CitiesProvider } from '../../_providers/cities-provider';
+import { RouterTestingModule } from '@angular/router/testing';
+import { RouteService } from '../../_services/route.service';
 
 describe('UserSubscriptionsComponent', () => {
   let component: UserSubscriptionsComponent;
@@ -15,10 +24,18 @@ describe('UserSubscriptionsComponent', () => {
         SubscriptionComponent
       ],
       imports: [
-        MaterialModule
+        MaterialModule,
+        HttpClientModule,
+        RouterTestingModule
       ],
       providers: [
-
+        SubscriptionService,
+        AuthService,
+        UserService,
+        DataProviderService,
+        CityService,
+        CitiesProvider,
+        RouteService
       ]
     })
       .compileComponents();
