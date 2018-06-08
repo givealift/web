15d670143f86532002228a99447186587716fbb8
Bartek Grodek <bartgrdk@gmail.com>
2018-06-08 11:22:06 +0200

    Implement searching for routes with interchanges

--- src/app/route/route-list/route-list.component.ts
+++ src/app/route/route-list/route-list.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Rx';
-import { Router } from "@angular/router";
 import { DataProviderService } from '../../_services/data-provider.service';
 import { ActivatedRoute } from '@angular/router';
 import { RouteService } from '../../_services/route.service';
+
 
 @Component({
   selector: 'app-route-list',
@@ -14,6 +12,8 @@ import { RouteService } from '../../_services/route.service';
 export class RouteListComponent implements OnInit {
 
   @Input() routes: any = [];
+  withInterchange: boolean = false;
+  nothingFound = false;
 
   constructor(
     private dataTransferService: DataProviderService,
@@ -26,14 +26,34 @@ export class RouteListComponent implements OnInit {
       const to = params['to'];
       const date = params['date'];
 
-      const taggedResults = this.dataTransferService.tagResults(from, to, date);
+      const tag = this.dataTransferService.tagResults(from, to, date);
 
-      let routes = this.dataTransferService.getData(`route-list/${taggedResults}`);
+      let data = this.dataTransferService.getData(`route-list/${tag}`);
+
+      let routes, withInterchange;
+      if (data !== null) {
+        ({ routes, withInterchange } = data);
+      } 
+
+      this.withInterchange = withInterchange;
 
       if (!routes) {
         this.routeService
-          .searchWithIds(+from, +to, date)
+          .searchByIds(+from, +to, date)
           .subscribe(routes => {
+            if (routes.length === 0) {
+              this.routeService
+                .searchByIdsWithInterchange(+from, +to, date)
+                .subscribe(routesWithChange => {
+                  if (routesWithChange.length === 0) {
+                    this.nothingFound = true;
+                  } else {
+                    this.withInterchange = true;
+                    this.routes = [].concat(...routesWithChange);
+                  }
+                })
+            }
+
             this.routes = routes;
           })
       }
