ae68cd5de028d0b5935d9672d1c92c4004205c5b
Thuleq <kszczyrbak8@gmail.com>
2018-06-09 20:21:43 +0200

    fixed user photo in rating

--- src/app/rating/rating.component.ts
+++ src/app/rating/rating.component.ts
@@ -1,19 +1,34 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { UserService } from '../_services/user.service';
+import { DomSanitizer } from '@angular/platform-browser';
+import { DataProviderService } from '../_services/data-provider.service';
 
 @Component({
   selector: 'app-rating',
   templateUrl: './rating.component.html',
   styleUrls: ['./rating.component.css']
 })
-export class RatingComponent {
+export class RatingComponent implements OnInit {
 
   rate: number;
 
+  sanitizedPhoto;
+
+
+
   constructor(private dialogRef: MatDialogRef<RatingComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private userService: UserService) {
+    private userService: UserService,
+    private sanitizer: DomSanitizer) {
+  }
+
+  ngOnInit() {
+    this.userService.getPhoto(this.data.userData.userId)
+      .subscribe(photo => {
+        let urlCreator = window.URL;
+        this.sanitizedPhoto = this.sanitizer.bypassSecurityTrustUrl(urlCreator.createObjectURL(photo));
+      })
   }
 
 
